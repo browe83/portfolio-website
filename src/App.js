b9ebb85d49ee75a65295994cc1d22b09c61c44f5
browe83 <rowe.brandon.j@gmail.com>
2021-01-12 14:32:32 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom';
 
-function App() {
+import Home from './components/Home';
+import Contact from './components/Contact';
+import NavBar from './components/NavBar';
+import NotFound from './components/NotFound';
+import Work from './components/Work';
+import About from './components/About';
+
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <NavBar />
+        <Switch>
+          <Route exact path='/' component={Home}></Route>
+          <Route exact path='/contact' component={Contact}></Route>
+          <Route exact path='/work' component={Work}></Route>
+          <Route exact path='/about' component={About}></Route>
+          <Route component={NotFound}></Route>
+        </Switch>
+      
+    </Router>
   );
 }
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
---
+++ src/components/Work.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Work (props) {
+  return <h1>Work</h1>
+}
+
+export default Work;
