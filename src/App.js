79a10670576a4ce9a7affb6da76489d18f658841
browe83 <rowe.brandon.j@gmail.com>
2021-02-08 14:11:29 -0500

    updates routes

--- src/App.js
+++ src/App.js
@@ -19,16 +19,18 @@ import Project from './components/Project';
 import Footer from './components/Footer';
 
 function App () {
+
+  console.log('env', process.env.PUBLIC_URL);
   return (
     <div className='page-container'>
       <div className='content-wrap'>
-        <Router>
+        <Router basename={process.env.PUBLIC_URL}>
           <NavBar />
             <Switch>
-              <Route exact path={process.env.PUBLIC_URL + '/'} component={Home}></Route>
-              <Route exact path={process.env.PUBLIC_URL + '/projects'} component={Work}></Route>
-              <Route exact path={process.env.PUBLIC_URL + '/project/:id'} component={Project}></Route>
-              <Route exact path={process.env.PUBLIC_URL + '/about'} component={About}></Route>
+              <Route exact path= '/' component={Home}></Route>
+              <Route exact path= '/projects' component={Work}></Route>
+              <Route exact path= '/project/:id' component={Project}></Route>
+              <Route exact path= '/about' component={About}></Route>
               <Route component={NotFound}></Route>
             </Switch>
         </Router>
